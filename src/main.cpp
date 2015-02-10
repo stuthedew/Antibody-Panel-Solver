07d0e43adbb22cfde1c1fb9a8e4c19ac366ce1ed
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2015-02-10 13:46:06 -0600

    Added parser source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-
-typedef enum{
-	D = 0,C,E,c,e,f,
-	C_w,V,M,N,S,s,P1,
-	Le_a,Le_b,Lu_a,
-	Lu_b,K,k,Kp_a,
-	Js_a,Fy_a,Fy_b,
-	Jk_a,Jk_b,Xg_a
-
-	
-}antibody_e;
-
-class Row{
-	public:
-		Row(){
-			
-		}
-		
-	private:
-		typedef struct antibody_t{
-			antibody_e epitope ;
-			bool const reaction ;
-			
-			antibody_t( antibody_e e, bool b):epitope( e ), reaction( b ){}		
-		}antibody_t;
-		
-	antibody_t* _screen;
-};
+#include <antibodyParser.h>
 
 using namespace std;
+Row test;
 int main(int argc, char *argv[]) {
 	ifstream file ( "data/screen4.csv" );
 	string value, dummyValue, subString;
@@ -44,5 +17,5 @@ int main(int argc, char *argv[]) {
 		cout << value; // display value removing the first and the last character from it
 		subString = value.substr();
 		}
-
+	cout << endl << test.testFunc();
 }
