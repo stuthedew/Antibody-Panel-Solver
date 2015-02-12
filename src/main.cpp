84b1f0407019fb71fdd65084636e4b9bae5541db
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2015-02-12 09:08:05 -0600

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -19,38 +19,31 @@ char header[] = "D\tC\tE\tc\te\tf\tC_w\tV\tM\tN\tS\ts\tP1\tLe_a\tLe_b\tLu_a\tLu_
 
 int main(int argc, char *argv[]) {
 
-
-
-
 	SampleFactory sf( "data/screen4.csv", "data/reflexPanel.csv", "data/Unknown1_4.csv");
 	TestSample * screen = sf.makeSamples(type_screen);
 	TestSample * reflex = sf.makeSamples(type_reflex);
 	bitset<26> y(Unknown.getPossibleAntigens());
-	//cout << y << endl;
 
 	if(screen[0].getReactionStatus() || screen[1].getReactionStatus()){
-	for(int i = 0; i < 2; i++){
-		//bitset<26> y(screen[i].getAntigens());
-		//cout << y << ", " << screen[i].getReactionStatus() << endl;
-		if(!screen[i].getReactionStatus()){
-			Unknown.eliminatePossibleAntigens(screen[i].getAntigens());
+		for(int i = 0; i < 2; i++){
+			if(!screen[i].getReactionStatus()){
+				Unknown.eliminatePossibleAntigens(screen[i].getAntigens());
+			}
 		}
+	}
 
-	}
-	}
-	//cout << endl;
 	y=Unknown.getPossibleAntigens();
-		//cout << y << endl;
-		//cout << endl;
+
+
 
 	for(int i = 0; i < 11; i++){
-			//bitset<26> y(reflex[i].getAntigens());
-			//cout << y << ", " << reflex[i].getReactionStatus() << endl;
+
+
 		if(!reflex[i].getReactionStatus()){
 					Unknown.eliminatePossibleAntigens(reflex[i].getAntigens());
 				}
 		}
-	//cout << endl;
+
 		y=Unknown.getPossibleAntigens();
 			string bit = y.to_string();
 			cout << header << endl;
